bccc6a6f541717617adc209258cba0506443fdda
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-05-26 23:47:47 -0400

    ref

--- lib/department.py
+++ lib/department.py
@@ -2,7 +2,7 @@ from __init__ import CURSOR, CONN
 
 
 class Department:
-
+    all = {}
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
@@ -22,7 +22,7 @@ class Department:
         """
         CURSOR.execute(sql)
         CONN.commit()
-
+ 
     @classmethod
     def drop_table(cls):
         """ Drop the table that persists Department instances """
@@ -30,7 +30,7 @@ class Department:
             DROP TABLE IF EXISTS departments;
         """
         CURSOR.execute(sql)
-        CONN.commit()
+        CONN.commit() 
 
     def save(self):
         """ Insert a new row with the name and location values of the current Department instance.
@@ -40,12 +40,11 @@ class Department:
             INSERT INTO departments (name, location)
             VALUES (?, ?)
         """
-
         CURSOR.execute(sql, (self.name, self.location))
         CONN.commit()
 
         self.id = CURSOR.lastrowid
-
+        type(self).all[self.id] = self
     @classmethod
     def create(cls, name, location):
         """ Initialize a new Department instance and save the object to the database """
@@ -72,3 +71,55 @@ class Department:
 
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
+        del type(self).all[self.id]
+        self.id = None
+    @classmethod
+    def instance_from_db(cls, row):
+        """Return a Department object having the attribute values from the table row."""
+
+        # Check the dictionary for an existing instance using the row's primary key
+        department = cls.all.get(row[0])
+        if department:
+            # ensure attributes match row values in case local object was modified
+            department.name = row[1]
+            department.location = row[2]
+        else:
+            # not in dictionary, create new instance and add to dictionary
+            department = cls(row[1], row[2])
+            department.id = row[0]
+            cls.all[department.id] = department
+        return department
+    @classmethod
+    def get_all(cls):
+        """Return a list containing a Department object per row in the table"""
+        sql = """
+            SELECT *
+            FROM departments
+        """
+
+        rows = CURSOR.execute(sql).fetchall()
+
+        return [cls.instance_from_db(row) for row in rows]
+    @classmethod
+    def find_by_id(cls, id):
+        """Return a Department object corresponding to the table row matching the specified primary key"""
+        sql = """
+            SELECT *
+            FROM departments
+            WHERE id = ?
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+    @classmethod
+    def find_by_name(cls, name):
+        """Return a Department object corresponding to first table row matching specified name"""
+        sql = """
+            SELECT *
+            FROM departments
+            WHERE name is ?
+        """
+
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+
